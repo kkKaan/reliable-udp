5f9ed928112e399eeca6f7135e80bdd4006658fa
Kaan Karaçanta <72619096+kkKaan@users.noreply.github.com>
2023-12-29 14:14:00 +0300

    Update server.py

--- ceng435-main/code/server.py
+++ ceng435-main/code/server.py
@@ -1,95 +1,65 @@
-#!/usr/bin/env python3
-
-"""
-This file is a Python script that starts the RDTOverUDP file transmission server first,
-and TCP file transmission server second.
-"""
-
 import sys
-
 from src import *
 
-# The command-line variables
-
 HOST = '0.0.0.0'
-UDP_PORT = 20001
-TCP_PORT = 65432
+TCP_PORT, UDP_PORT = 65432, 20001
 
-
+# Calculates the average transmission time for each package and the total transmission time,
+# returns a tuple containing the average time and total transmission time in milliseconds.
 def calculate_avg_and_total_time(timestamps):
-    """Calculates average time for each package and total transmission time for given timestamps"""
+    """
+    :param timestamps: A list of timestamps representing sent and received times of packages.
+    """
+    if not timestamps:
+        return 0, 0  # Return 0 if there are no timestamps
 
-    # If there is no timestamp, just return 0,0
-    if len(timestamps) == 0: return 0, 0
+    total_time = (timestamps[-1] - timestamps[0]) * 1000  # Total time in milliseconds
 
-    # Calculate the total time passed for file transmission
-    total_time = (timestamps[-1] - timestamps[0]) * 1000
-
-    # Calculate the difference between time sent and time received of packages
-    time_diffs = []
-    for i in range(0, len(timestamps), 2):
-        time_diffs.append(abs(timestamps[i + 1] - timestamps[i]) * 1000)
-
-    # Calculate the average of the time differences
-    avg_time = sum(time_diffs) / len(time_diffs)
+    # Calculate differences between sent and received times
+    time_diffs = [abs(timestamps[i + 1] - timestamps[i]) * 1000 for i in range(0, len(timestamps), 2)]
+    avg_time = sum(time_diffs) / len(time_diffs)  # Average time in milliseconds
 
     return avg_time, total_time
 
-
-def receive_rdt_over_udp():
-    """Starts a RDPOverUDP file transmission server to receive a file"""
-
+# Starts an RDTOverUDP file transmission server to receive a file.
+def receive_rdt_over_udp(host, udp_port, output_filename):
+    """
+    :param host: The host IP address.
+    :param udp_port: The UDP port number for the server.
+    :param output_filename: The filename to save the received data.
+    """
     timestamps = []
 
-    result_file = open('transfer_file_UDP.txt', 'wb') # Open the file
-
-    server = RDTOverUDPServer(HOST, UDP_PORT) # Initialize server
-
-    # Receive package_data for every chunk of file
-    for package in server.process():
-        # Save the sent and received time for packages
-        timestamps.append(package.timestamp_sent)
-        timestamps.append(package.timestamp_received)
-
-        # Write chunk into the file
-        result_file.write(package.chunk)
-
-    result_file.close() # Close the file
+    with open(output_filename, 'wb') as result_file:
+        server = RDTOverUDPServer(host, udp_port)
+        for package in server.process():
+            timestamps.extend([package.timestamp_sent, package.timestamp_received])
+            result_file.write(package.chunk)
 
     avg_time, total_time = calculate_avg_and_total_time(timestamps)
+    print('UDP Packets Average Transmission Time: {:.6f} ms'.format(avg_time))
+    print('UDP Communication Total Transmission Time: {:.6f} ms'.format(total_time))
 
-    # Print the calculated values
-    print ('UDP Packets Average Transmission Time:', '%.6f' % avg_time , 'ms')
-    print ('UDP Communication Total Transmission Time:', '%.6f' % total_time, 'ms')
-
-
-def receive_tcp():
-    """Starts a TCP file transmission server to receive a file"""
-
+# Starts a TCP file transmission server to receive a file.
+def receive_tcp(host, tcp_port, output_filename):
+    """
+    :param host: The host IP address.
+    :param tcp_port: The TCP port number for the server.
+    :param output_filename: The filename to save the received data.
+    """
     timestamps = []
 
-    result_file = open('transfer_file_TCP.txt', 'wb') # Open the file
-
-    server = TCPServer(HOST, TCP_PORT) # Initialize server
-
-    # Receive timestamp and chunk for every chunk of file
-    for timestamp_sent, timestamp_received, chunk in server.process():
-        # Save the sent and received time for packages
-        timestamps.append(timestamp_sent)
-        timestamps.append(timestamp_received)
-
-        # Write chunk into the file
-        result_file.write(chunk)
-
-    result_file.close() # Close the file
+    with open(output_filename, 'wb') as result_file:
+        server = TCPServer(host, tcp_port)
+        for timestamp_sent, timestamp_received, chunk in server.process():
+            timestamps.extend([timestamp_sent, timestamp_received])
+            result_file.write(chunk)
 
     avg_time, total_time = calculate_avg_and_total_time(timestamps)
-
-    # Print the calculated values
-    print ('TCP Packets Average Transmission Time:', '%.6f' % avg_time , 'ms')
-    print ('TCP Communication Total Transmission Time:', '%.6f' % total_time, 'ms')
-
+    print('TCP Packets Average Transmission Time: {:.6f} ms'.format(avg_time))
+    print('TCP Communication Total Transmission Time: {:.6f} ms'.format(total_time))
 
 if __name__ == '__main__':
-    receive_rdt_over_udp()
-    # receive_tcp()
+    receive_rdt_over_udp(HOST, UDP_PORT, 'transfer_file_UDP.txt')
+    # receive_tcp(HOST, TCP_PORT, 'transfer_file_TCP.txt')
+
