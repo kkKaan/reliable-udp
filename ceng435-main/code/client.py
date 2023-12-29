0b6e85e76bcfb182ef11fd432c0cf5e5572e6f09
Kaan Karaçanta <72619096+kkKaan@users.noreply.github.com>
2023-12-29 13:59:05 +0300

    Update client.py

--- ceng435-main/code/client.py
+++ ceng435-main/code/client.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python3
-
-"""
-This file is a Python script that starts the RDTOverUDP file transmission to server first,
-and TCP file transmission to server second.
-"""
-
 import sys
 import time
 
 from src import *
-
-# The command-line variables
 
 TARGET_HOST = "172.17.0.2"
 UDP_TARGET_PORT = 20001
@@ -18,74 +9,49 @@ TCP_TARGET_PORT = 65432
 UDP_SENDER_PORT = 20001
 TCP_SENDER_PORT = 65432
 
+# Splits a file into chunks, yields the chunks respectively.
+def chunk_file(filename, chunk_size):
+    """
+    :param filename: The path to the file to be chunked.
+    :param chunk_size: The size of each chunk.
+    """
+    with open(filename, 'rb') as file:
+        while True:
+            chunk = file.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
 
-def chunk_rdt_over_udp_file(filename):
-    """The generator function to split chunks of the file given with its filename"""
+# Sends the file data using the RDTOverUDP protocol.
+def send_rdt_over_udp(filename, sender_port, target_host, target_port):
+    """
+    :param filename: Name of the file to be sent.
+    :param sender_port: Port number for the UDP client.
+    :param target_host: Target host address.
+    :param target_port: Target port number.
+    """
+    data = chunk_file(filename, RDT_OVER_UDP_FILE_READ_SIZE)
+    client = RDTOverUDPClient(sender_port, target_host, target_port, data)
 
-    file = open(filename, 'rb')
+    retransmission_count = client.process()
+    print('UDP Transmission Re-transferred Packets:', retransmission_count)
 
-    while True:
-        # RDT_OVER_UDP_FILE_READ_SIZE is the maximum chunk size to read
-        # in order to fit the PACKAGE_SIZE defined in constant.py
-        chunk = file.read(RDT_OVER_UDP_FILE_READ_SIZE)
-
-        # chunk is None, end of the file
-        if not chunk:
-            break
-
-        # Yield the chunk
-        yield chunk
-
-    # Yield and empty chunk to state that the end of file is received.
-    # This line is required for the last package of RDTOverUDP protocol.
-    yield b''
-
-    file.close() # Close the file
-
-
-def chunk_tcp_file(filename):
-    file = open(filename, 'rb')
-
-    while True:
-        # TCP_FILE_READ_SIZE is the maximum chunk size to read
-        # in order to fit the PACKAGE_SIZE defined in constant.py
-        chunk = file.read(TCP_FILE_READ_SIZE)
-
-        # chunk is None, end of the file
-        if not chunk:
-            break
-
-        # Yield the chunk
-        yield chunk
-
-    file.close() # Close the file
-
-
-def send_rdt_over_udp():
-    """Sends the file data using RDTOverUDP protocol"""
-
-    data = chunk_rdt_over_udp_file('small-0.obj')
-
-    client = RDTOverUDPClient(UDP_SENDER_PORT, TARGET_HOST, UDP_TARGET_PORT, data)
-
-    c = client.process() # Process and send the file and receive retransmission packet count
-
-    print ('UDP Transmission Re-transferred Packets:', c)
-
-
-def send_tcp():
-    """Sends the file data using TCP protocol"""
-
-    data = chunk_tcp_file('transfer_file_TCP.txt')
-
-    client = TCPClient(TCP_SENDER_PORT, TARGET_HOST, TCP_TARGET_PORT, data)
+# Sends the file data using the TCP protocol.
+def send_tcp(filename, sender_port, target_host, target_port):
+    """
+    :param filename: Name of the file to be sent.
+    :param sender_port: Port number for the TCP client.
+    :param target_host: Target host address.
+    :param target_port: Target port number.
+    """
+    data = chunk_file(filename, TCP_FILE_READ_SIZE)
+    client = TCPClient(sender_port, target_host, target_port, data)
 
     client.process()
 
-
 if __name__ == '__main__':
-    time.sleep(1) # Wait one second for RDTOverUDPServer start
-    send_rdt_over_udp()
+    time.sleep(1)  # Wait for RDTOverUDPServer to start
+    send_rdt_over_udp('small-0.obj', UDP_SENDER_PORT, TARGET_HOST, UDP_TARGET_PORT)
 
-    time.sleep(1) # Wait one second for TCPServer start
-    # send_tcp()
+    time.sleep(1)  # Wait for TCPServer to start
+    # send_tcp('transfer_file_TCP.txt', TCP_SENDER_PORT, TARGET_HOST, TCP_TARGET_PORT)
